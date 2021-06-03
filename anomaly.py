35a36eae8dc04f747cb746e219dc0e2d6b4ff59e
Philippe Partarrieu <ppartarr@outlook.com>
2021-06-03 09:32:25 +0200

    working iForest impl
    add anomaly score per day & working rocauc

--- anomaly.py
+++ anomaly.py
@@ -5,8 +5,9 @@ from sklearn.ensemble import IsolationForest
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
 import numpy as np
 import pandas as pd
 import argparse
@@ -21,33 +22,31 @@ log.basicConfig(format='%(asctime)s.%(msecs)06d: %(message)s',
 
 
 def train(x, y):
-
+    """Train the model and calculate performance metrics"""
     # split dataset into train & test
-    # TODO test stratification
     x_train, x_test, y_train, y_test = train_test_split(
-        x, y, train_size=0.5, test_size=0.5, shuffle=False)
-
-    log.info(x_train.shape)
-    log.info(x_test.shape)
+        x, y, test_size=0.2, shuffle=True)
 
     # model_tuning(x_train, y_train)
 
-    # TODO test bootstrapping
     iforest = IsolationForest(n_estimators=80,
         max_features=30,
-        verbose=2)
+        verbose=1)
 
-    x_pred_train = iforest.fit_predict(x_train,)
-    x_pred_test = iforest.fit_predict(x_test)
-    log.info(x_pred_train.shape)
-    log.info(np.unique(x_pred_train, return_counts=True))
-    log.info(x_pred_test.shape)
-    log.info(np.unique(x_pred_test, return_counts=True))
+    estimator = iforest.fit(x_train)
+    guesses = estimator.predict(x_test)
+
+    log.info('guess percentage of anomalies: {percentage:.2f}'.format(percentage=(100 * np.count_nonzero(guesses == -1)) / len(guesses)))
+    log.info('actual percentage of anomalies: {percentage:.2f}'.format(percentage=((len(y) - y.value_counts()[1])*100) / y.value_counts()[1]))
+
+    auc = roc_auc_score(y_test, guesses)
+    print('area under the curve: {auc}'.format(auc=auc))
 
 
-""" Tune the model by testing various hyperparameters using the GridSearchCV
-"""
+
 def model_tuning(x_train, y_train):
+    """ Tune the model by testing various hyperparameters using the GridSearchCV"""
+
     iforest = IsolationForest(verbose=1)
 
     param_grid = {'n_estimators': [40, 60, 80],
@@ -59,7 +58,7 @@ def model_tuning(x_train, y_train):
 
     grid_search = GridSearchCV(iforest,
         param_grid,
-        scoring="neg_mean_squared_error",
+        scoring="roc_auc_score",
         refit=True,
         return_train_score=True,
         verbose=1)
@@ -70,64 +69,80 @@ def model_tuning(x_train, y_train):
     print('Best parameters', best_model.best_params_)
 
 
-def process_data(filepath):
-    log.info('Opening {}...'.format(filepath))
-    raw_data = pd.read_csv(filepath)
-    
-    y = raw_data['Label']
+def process_labels(y):
+    """Convert the labels into numerical values"""
+
+    # set all malicious labels to -1
     y.replace('Bot', -1, inplace=True)
+    y.replace('DoS GoldenEye', -1, inplace=True)
+    y.replace('Heartbleed', -1, inplace=True)
+    y.replace('DoS Hulk', -1, inplace=True)
+    y.replace('DoS Slowhttp', -1, inplace=True)
+    y.replace('DoS slowloris', -1, inplace=True)
+    y.replace('SSH-Patator', -1, inplace=True)
+    y.replace('FTP-Patator', -1, inplace=True)
+    y.replace('Web Attack', -1, inplace=True)
+    y.replace('Infiltration', -1, inplace=True)
+    y.replace('PortScan', -1, inplace=True)
+    y.replace('DDoS', -1, inplace=True)
+
+    # set normal label to 1
     y.replace('Benign', 1, inplace=True)
 
-    x = raw_data.drop(['Label'], axis=1)
+    return y
 
-    # TODO convert timestamp to unix timestamp
-    x = x.drop(['Timestamp'], axis=1)
 
-    # find columns with NaN value and replace with 0
-    # Flow Byts/s
-    nan_columns = x.loc[:, x.isna().any()].columns
-    for column in nan_columns:
-        x[column].fillna(0, inplace=True)
-
-    # find columns with Infinity value and replace with 0
-    # Flow Byts/s
-    # Flow Pkts/s
-    inf_columns = x.columns.to_series()[np.isinf(x).any()]
+def process_infinity(x):
+    """Replace all the Infinity values with the column's max"""
+    inf_columns = x.columns[np.isinf(x).any()]
     for column in inf_columns:
         # replace Infinity with column max
         inf = x.loc[x[column] != np.inf, column].max()
         x[column].replace(np.inf, inf, inplace=True)
+    
+    return x
+
+
+def process_nan(x):
+    """Replace all the NaN values with the column's median"""
+    nan_columns = x.loc[:, x.isna().any()].columns
+    for column in nan_columns:
+        x[column].fillna(0, inplace=True)
+    # imputer = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+    # x = pd.DataFrame(data=imputer.transform(x.values), columns=x.columns)
+    return x
+
+
+def process_data(filepath):
+    """Ingest the raw csv data and run pre-processing tasks"""
+
+    log.info('Opening {}...'.format(filepath))
+    raw_data = pd.read_csv(filepath)
+
+    y = process_labels(raw_data['Label'])
+
+    # TODO convert timestamp to unix timestamp
+    x = raw_data.drop(['Timestamp'], axis=1)
+
+    x.drop(['Label'], axis=1, inplace=True)
+    x = process_nan(x)
+    x = process_infinity(x)
 
     # find columns with constant values and drop
-    # Bwd PSH Flags
-    # Fwd URG Flags
-    # Bwd URG Flags
-    # CWE Flag Count
-    # Fwd Byts/b Avg
-    # Fwd Pkts/b Avg
-    # Fwd Blk Rate Avg
-    # Bwd Byts/b Avg
-    # Bwd Pkts/b Avg
-    # Bwd Blk Rate Avg
     for column in x.columns:
         if len(x[column].value_counts()) == 1:
             x = x.drop([column], axis=1)
-
-    log.info(x.head())
 
     return x, y
 
 
 def parse_args():
+    """Parse command line arguments"""
     parser = argparse.ArgumentParser(description='Anomanly-based Network Intrusion Detection')
     parser.add_argument('--data', help='.csv file to read flow data from', required=True)
     return parser.parse_args()
 
 
-# >>> data['Label'].value_counts()
-# Benign    762384
-# Bot       286191
-# Total of 37.54% malicious packets...
 if __name__ == '__main__':
     args = parse_args()
     x, y = process_data(args.data)
--- readme.md
+++ readme.md
@@ -15,3 +15,20 @@ We are using argparse, here is an example of how to run the script:
 ```bash
 python3 anomaly.py --data ./data/Friday-02-03-2018_TrafficForML_CICFlowMeter.csv
 ```
+
+## Anomaly count
+```bash
+λ  philippe@srv11013 9:22:48  /mnt/storage/ids-2018/dataset  python3 stats.py --dir Processed\ Traffic\ Data\ for\ ML\ Algorithms
+Processed Traffic Data for ML Algorithms/Wednesday-21-02-2018_TrafficForML_CICFlowMeter.csv percentage of malicious flows: 190.5984208761394
+Processed Traffic Data for ML Algorithms/Thursday-15-02-2018_TrafficForML_CICFlowMeter.csv percentage of malicious flows: 5.270476077652631
+Processed Traffic Data for ML Algorithms/Friday-02-03-2018_TrafficForML_CICFlowMeter.csv percentage of malicious flows: 37.53895674620664
+sys:1: DtypeWarning: Columns (0,1,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78) have mixed types.Specify dtype option on import or set low_memory=False.
+Processed Traffic Data for ML Algorithms/Thursday-01-03-2018_TrafficForML_CICFlowMeter.csv percentage of malicious flows: 39.10652545612657
+Processed Traffic Data for ML Algorithms/Wednesday-14-02-2018_TrafficForML_CICFlowMeter.csv percentage of malicious flows: 57.06024031418788
+Processed Traffic Data for ML Algorithms/Friday-16-02-2018_TrafficForML_CICFlowMeter.csv percentage of malicious flows: 134.70024979184015
+Processed Traffic Data for ML Algorithms/Wednesday-28-02-2018_TrafficForML_CICFlowMeter.csv percentage of malicious flows: 12.661521499448734
+Processed Traffic Data for ML Algorithms/Thursday-22-02-2018_TrafficForML_CICFlowMeter.csv percentage of malicious flows: 0.03453496569876542
+Processed Traffic Data for ML Algorithms/Thuesday-20-02-2018_TrafficForML_CICFlowMeter.csv percentage of malicious flows: 7.815348189237466
+Processed Traffic Data for ML Algorithms/Friday-23-02-2018_TrafficForML_CICFlowMeter.csv percentage of malicious flows: 0.05400716978575566
+total percentage of malicious flows: 20.380819517930977
+```
