5607ff4830d4ec3acaf84a669232839f71b603c6
Philippe Partarrieu <ppartarr@outlook.com>
2021-06-07 21:48:41 +0200

    add gradient boost model

--- anomaly.py
+++ anomaly.py
@@ -5,10 +5,9 @@ from sklearn.ensemble import IsolationForest
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.metrics import roc_auc_score, plot_roc_curve
 from sklearn.preprocessing import StandardScaler
-from sklearn.impute import SimpleImputer
 from sklearn.feature_selection import SelectKBest
 from sklearn.mixture import GaussianMixture
-from model import train_gmm, train_iforest, gmm_tuning, iforest_tuning
+from model import train_gmm, train_iforest, train_gboost, gmm_tuning, iforest_tuning
 from utils import process_data
 
 import numpy as np
@@ -28,7 +27,7 @@ def train(x, y):
     """Train the model and calculate performance metrics"""
     # split dataset into train & test
     x_train, x_test, y_train, y_test = train_test_split(
-        x, y, test_size=0.2, shuffle=True)
+        x, y, test_size=0.2, shuffle=False)
 
     # print(find_best_features(x, x_train, y_train))
     # model_tuning(x_train, y_train)
@@ -37,6 +36,7 @@ def train(x, y):
 
     train_iforest(x, y, x_train, x_test, y_train, y_test)
     # train_gmm(x, y, x_train, x_test, y_train, y_test)
+    # train_gboost(x, y, x_train, x_test, y_train, y_test)
 
 
 def plot(x, y, guesses, col_name):
--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 #!/home/philippe/src/anomaly/venv/bin/python3
 # coding: utf-8
 
-from sklearn.ensemble import IsolationForest
+from sklearn.ensemble import IsolationForest, GradientBoostingClassifier
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.metrics import roc_auc_score, silhouette_score
 from sklearn.preprocessing import StandardScaler
@@ -11,9 +11,24 @@ from sklearn.mixture import GaussianMixture
 import numpy as np
 import logging as log
 
+from utils import print_stats
+
 
 log.basicConfig(format='%(asctime)s.%(msecs)06d: %(message)s',
                 datefmt='%Y-%m-%d %H:%M:%S', level=log.INFO)
+
+
+def train_gboost(x, y, x_train, x_test, y_train, y_test):
+    """Train & test the Gradient Boosting Model"""
+    log.info('Training the Gradient Boosting Model')
+    gboost = GradientBoostingClassifier(
+        n_estimators=80,
+        verbose=1)
+
+    classifier = gboost.fit(x_train, y_train)
+    guesses = classifier.predict(x_test)
+
+    print_stats(y, guesses, y_test)
 
 
 def train_gmm(x, y, x_train, x_test, y_train, y_test):
